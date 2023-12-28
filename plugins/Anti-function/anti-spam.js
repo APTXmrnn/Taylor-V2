47c9280bc42a16c9693a5bec3264dbf73fa3b12f
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-12-28 17:14:25 +0800

    Add files via upload

--- plugins/Anti-function/anti-spam.js
+++ plugins/Anti-function/anti-spam.js
@@ -2,38 +2,54 @@ import {
     performance
 } from 'perf_hooks';
 
+const SPAM_THRESHOLD = 5;
+const COOLDOWN_DURATION = 10000;
+const BAN_DURATION = 5000;
+
 export async function before(m) {
     const users = global.db.data.users;
     const chats = global.db.data.chats;
 
-    if (!chats[m.chat].antiSpam || m.isBaileys || m.mtype === 'protocolMessage' || m.mtype === 'pollUpdateMessage' || m.mtype === 'reactionMessage') return;
-    if (!m.msg || !m.message || m.key.remoteJid !== m.chat || users[m.sender].banned || chats[m.chat].isBanned) return;
+    if (
+        chats[m.chat].antiSpam &&
+        !m.isBaileys &&
+        !['protocolMessage', 'pollUpdateMessage', 'reactionMessage'].includes(m.mtype) &&
+        m.msg &&
+        m.message &&
+        m.key.remoteJid === m.chat &&
+        !users[m.sender].banned &&
+        !chats[m.chat].isBanned
+    ) {
+        this.spam = this.spam || {};
+        this.spam[m.sender] = this.spam[m.sender] || {
+            count: 0,
+            lastspam: 0
+        };
 
-    this.spam = this.spam || {};
-    this.spam[m.sender] = this.spam[m.sender] || {
-        count: 0,
-        lastspam: 0
-    };
-    const now = performance.now();
-    const timeDifference = now - this.spam[m.sender].lastspam;
+        const now = performance.now();
+        const timeDifference = now - this.spam[m.sender].lastspam;
 
-    if (timeDifference < 10000) {
-        this.spam[m.sender].count++;
-        if (this.spam[m.sender].count >= 5) {
-            users[m.sender].banned = true;
-            this.spam[m.sender].lastspam = now + 5000;
-            const remainingCooldown = Math.ceil((this.spam[m.sender].lastspam - now) / 1000);
-            setTimeout(() => {
-                users[m.sender].banned = false;
-                this.spam[m.sender].count = 0;
-                m.reply(`✅ *Cooldown selesai*\nAnda bisa mengirim pesan lagi.`);
-            }, 5000);
-            const message = m.mtype.replace(/message$/i, '').replace('audio', m.msg.ptt ? 'PTT' : 'audio').replace(/^./, v => v.toUpperCase()) || 'Unknown';
-            return m.reply(`❌ *Mohon jangan spam ${message}*\nTunggu setelah ${remainingCooldown} detik`);
+        if (timeDifference < COOLDOWN_DURATION) {
+            this.spam[m.sender].count++;
+
+            if (this.spam[m.sender].count >= SPAM_THRESHOLD) {
+                users[m.sender].banned = true;
+                this.spam[m.sender].lastspam = now + BAN_DURATION;
+
+                setTimeout(() => {
+                    users[m.sender].banned = false;
+                    this.spam[m.sender].count = 0;
+                    m.reply(`✅ *Cooldown selesai*\nAnda bisa mengirim pesan lagi.`);
+                }, BAN_DURATION);
+
+                const remainingCooldown = Math.ceil((this.spam[m.sender].lastspam - now) / 1000);
+                const message = m.mtype.replace(/message$/i, '').replace('audio', m.msg.ptt ? 'PTT' : 'audio').replace(/^./, v => v.toUpperCase()) || 'Unknown';
+                return m.reply(`❌ *Mohon jangan spam ${message}*\nTunggu setelah ${remainingCooldown} detik`);
+            }
+        } else {
+            this.spam[m.sender].count = 0;
         }
-    } else {
-        this.spam[m.sender].count = 0;
-    }
 
-    this.spam[m.sender].lastspam = now;
+        this.spam[m.sender].lastspam = now;
+    }
 }
